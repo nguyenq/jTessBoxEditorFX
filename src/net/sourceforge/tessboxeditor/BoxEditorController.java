5b8930eb5c169af81a173e760be370b8382705b5
Quan Nguyen <nguyenq87@gmail.com>
2026-06-03 17:29:06 -0500

    Update dep

--- src/net/sourceforge/tessboxeditor/BoxEditorController.java
+++ src/net/sourceforge/tessboxeditor/BoxEditorController.java
@@ -24,8 +24,6 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
